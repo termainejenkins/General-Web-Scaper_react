b2d3b615a546c3faea61b0f29a337005c35ccdd1
TJ <termainejenkins@gmail.com>
2023-12-03 21:20:07 -0500

    Fixed the cors issue

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,23 @@
-// server.js
 import express from 'express';
 import axios from 'axios';
 import bodyParser from 'body-parser';
-import cors from 'cors'; // Import cors package
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const PORT = 8000;
 const app = express();
 
-app.use(cors()); // Enable CORS
 app.use(bodyParser.json());
 
+// This is a proxy route for the /scrape endpoint
+app.use(
+  '/scrape',
+  createProxyMiddleware({
+    target: 'http://localhost:8000',  // This is the backend server's URL
+    changeOrigin: true,
+  })
+);
+
+// Express Server route for existing /scrape route
 app.post('/scrape', async (req, res) => {
   const { url } = req.body;
 
@@ -27,6 +35,5 @@ app.post('/scrape', async (req, res) => {
     res.status(500).json({ error: 'An error occurred during scraping.', details: error.message });
   }
 });
-
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
